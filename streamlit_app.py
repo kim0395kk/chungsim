8a11f8b9c60c3534bc249305c95816e9feecd0d9
kim0395kk <138691225+kim0395kk@users.noreply.github.com>
2026-01-11 23:49:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -795,7 +795,7 @@ def main():
 
     with col_left:
         st.title("🏢 AI 행정관 Pro 충주시청")
-        st.caption("문의 kim0395kk@korea.kr Gemini + 국가법령정보 + Naver Search + Strategy + DB")
+        st.caption("문의 kim0395kk@korea.kr /n 세계최초 행정 Govable AI 에이젼트 ")
         st.markdown("---")
 
         st.markdown("### 🗣️ 업무 지시")
